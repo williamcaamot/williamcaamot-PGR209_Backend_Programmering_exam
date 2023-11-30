71d9f0943f4d5eff43953d2ad338b3f847afb210
Markushagen1 <57091267+Markushagen1@users.noreply.github.com>
2023-11-30 16:28:37 +0100

    start on customer

--- src/main/java/com/example/exam/Model/Customer.java
+++ src/main/java/com/example/exam/Model/Customer.java
@@ -1,4 +1,34 @@
 package com.example.exam.Model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_seq_gen")
+    @SequenceGenerator(name = "customer_seg_gen", sequenceName = "customer_seq", allocationSize = 1)
+    @Column(name = "customer_id")
+    private long customerId = 0L;
+
+    @Column(name = "customer_name")
+    private String customerName;
+
+    @Column(name = "customer_email")
+    private String customerEmail;
+
+    //TODO; add relations
+
+
+    public Customer(String customerName, String customerEmail){
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+    }
 }
