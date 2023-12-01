7e6d52cc3deda33f20d1cae869e76ce4f6607980
williamdev <william-ca@live.no>
2023-12-01 11:25:40 +0100

    Service start

--- src/main/java/com/example/exam/Service/AddressService.java
+++ src/main/java/com/example/exam/Service/AddressService.java
@@ -1,7 +1,25 @@
 package com.example.exam.Service;
 
+import com.example.exam.Controller.AddressController;
+import com.example.exam.Model.Address;
+import com.example.exam.Repo.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddressService {
+
+    AddressRepository addressRepository;
+
+    @Autowired
+    public AddressService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+
+    public Address getOneById(){
+        return new Address();
+    }
+
+
 }
