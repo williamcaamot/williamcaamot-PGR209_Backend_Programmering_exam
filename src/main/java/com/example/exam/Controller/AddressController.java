5ddd7558db545c2e971b3657f83f463d7aa368a0
williamdev <william-ca@live.no>
2023-12-01 13:34:00 +0100

    Fix controller

--- src/main/java/com/example/exam/Controller/AddressController.java
+++ src/main/java/com/example/exam/Controller/AddressController.java
@@ -1,6 +1,7 @@
 package com.example.exam.Controller;
 
 import com.example.exam.Model.Address;
+import com.example.exam.Model.Customer;
 import com.example.exam.Service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,15 +22,29 @@ public class AddressController {
 
 
     @GetMapping("")
-    public List<Address> getAddress(){
-        List<Address> addressList = addressService.getAddress();
-        System.out.println(addressList.get(0).getCustomers().get(0).getCustomerName());
+    public List<Address> getAddress() {
         return addressService.getAddress();
     }
 
+    @GetMapping("/{id}")
+    public Address getAddress(@PathVariable Long id) {
+        return addressService.getAddressById(id);
+    }
+
     @PostMapping("")
-    public Address addAddress(@RequestBody Address address){
+    public Address addAddress(@RequestBody Address address) {
         return addressService.addAddress(address);
     }
 
+    @DeleteMapping("")
+    public void deleteAddress(@RequestBody Address address) {
+        addressService.deleteAddress(address);
+        return;
+    }
+
+    @PutMapping("")
+    public Address updateAddress(@RequestBody Address address) {
+        return addressService.updateAddress(address);
+
+    }
 }
