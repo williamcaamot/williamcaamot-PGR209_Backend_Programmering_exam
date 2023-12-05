89b7e4b138e22190fe37019445f0272553e43560
williamdev <william-ca@live.no>
2023-12-05 21:01:47 +0100

    More testing, need to fix update sub assemnbly test

--- src/test/java/com/example/exam/subassembly/SubassemblyEndToEndTest.java
+++ src/test/java/com/example/exam/subassembly/SubassemblyEndToEndTest.java
@@ -14,8 +14,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -79,15 +79,99 @@ public class SubassemblyEndToEndTest {
         assertEquals(subassembly.getSubassemblyDescription(), addedSubassembly.getSubassemblyDescription());
         assertEquals(subassembly.getSubassemblyName(), addedSubassembly.getSubassemblyName());
 
+
+        //Cleanup
+        subassemblyRepository.delete(addedSubassembly);
+
     }
 
     //Add part to subassembly
+    @Test
+    void shouldAddPartToSubassembly() throws Exception{
+        MvcResult res = mockMvc.perform(post("/api/subassembly/1/part/1"))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        String resString = res.getResponse().getContentAsString();
+        Subassembly updatedSubassembly = objectMapper.readValue(resString, Subassembly.class);
+
+
+        assertNotNull(updatedSubassembly.getSubassemblyId());
+        assertNotNull(updatedSubassembly.getSubassemblyName());
+        assertNotNull(updatedSubassembly.getSubassemblyDescription());
+        assertNotNull(updatedSubassembly.getParts());
+        assertNotNull(updatedSubassembly.getParts().get(0).getPartId());
+        assertNotNull(updatedSubassembly.getParts().get(0).getPartName());
+        assertNotNull(updatedSubassembly.getParts().get(0).getPartDescription());
+    }
 
     //Delete subassembly
+    @Test
+    void shouldAddSubassemblyThenDeleteIt() throws Exception{
 
-    //Wrap in try catch and expect it to throw and exception? Does the delete method even check if it already exists? Then if not that should be implemented
+        //First adding a subassembly
+        Subassembly subassembly = new Subassembly("Subassembly Name", "Subassembly Description");
+        String subassemblyJson = objectMapper.writeValueAsString(subassembly);
+
+        MvcResult addedRes = mockMvc.perform(post("/api/subassembly")
+                        .contentType("application/json")
+                        .content(subassemblyJson))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        String addedResString = addedRes.getResponse().getContentAsString();
+        Subassembly addedSubassembly = objectMapper.readValue(addedResString, Subassembly.class);
+        String subassemblyToDeleteJson = objectMapper.writeValueAsString(addedSubassembly);
+
+
+        //Deleting the subassembly added
+        MvcResult deletedRes = mockMvc.perform(delete("/api/subassembly")
+                        .contentType("application/json")
+                        .content(subassemblyToDeleteJson))
+                .andExpect(status().isOk())
+                .andReturn();
+
+
+        //Trying to get the subassembly that was removed by ID, should return 404 not found
+        MvcResult res = mockMvc.perform(get("/api/subassembly/" + addedSubassembly.getSubassemblyId()))
+                .andExpect(status().isNotFound())
+                .andReturn();
+
+
+
+    }
+
 
     //Update subassembly
+    @Test
+    void shouldAddSubassemblyThenUpdateIt() throws Exception{
+        //First adding a subassembly
+        Subassembly subassembly = new Subassembly("Subassembly Name", "Subassembly Description");
+        Subassembly updateSubassembly = new Subassembly("Updated subassembly name", "updated subassmebly description");
+        String subassemblyJson = objectMapper.writeValueAsString(subassembly);
+        String updateSubassemblyJson = objectMapper.writeValueAsString(updateSubassembly);
 
+        MvcResult addedRes = mockMvc.perform(post("/api/subassembly")
+                        .contentType("application/json")
+                        .content(subassemblyJson))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+
+        String addedResJson = addedRes.getResponse().getContentAsString();
+        Subassembly addedSubassembly = objectMapper.readValue(addedResJson, Subassembly.class);
+        updateSubassembly.setSubassemblyId(addedSubassembly.getSubassemblyId());
+
+        MvcResult updatedRes = mockMvc.perform(put("/api/subassembly")
+                .contentType("application/json")
+                .content(updateSubassemblyJson))
+                .andExpect(status().isOk())
+                .andReturn();
+
+//TODO FIX THIS
+
+
+
+    }
 
 }
