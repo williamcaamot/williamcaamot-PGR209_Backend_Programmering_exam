20f5bd209b56331574e8b5ef6872a594dcb85d1a
Markushagen1 <57091267+Markushagen1@users.noreply.github.com>
2023-12-07 12:35:01 +0100

    getById tests

--- src/test/java/com/example/exam/subassembly/SubassemblyUnitTest.java
+++ src/test/java/com/example/exam/subassembly/SubassemblyUnitTest.java
@@ -9,7 +9,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.List;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -22,11 +26,29 @@ public class SubassemblyUnitTest {
     private SubassemblyService SubassemblyService;
 
     @Test
-    void shouldFetchSubassemblyes(){
+    void shouldFetchSubassemblies(){
         List<Subassembly> SubassemblyList = List.of(new Subassembly(), new Subassembly());
         when(SubassemblyRepository.findAll()).thenReturn(SubassemblyList);
 
         var subassemblies = SubassemblyService.getSubassembly();
         assert subassemblies.size() == 2;
     }
+
+    @Test
+    public void getSubassemblyById(){
+        Long exampleId = 1L;
+        Subassembly mockSubassembly = new Subassembly();
+
+        mockSubassembly.setSubassemblyId(exampleId);
+
+        when(SubassemblyRepository.findById(exampleId)).thenReturn(Optional.of(mockSubassembly));
+
+        Subassembly found = SubassemblyService.getSubassemblyById(exampleId);
+
+        assertNotNull(found);
+        assertEquals(exampleId, found.getSubassemblyId());
+
+        verify(SubassemblyRepository).findById(exampleId);
+    }
+    
 }
