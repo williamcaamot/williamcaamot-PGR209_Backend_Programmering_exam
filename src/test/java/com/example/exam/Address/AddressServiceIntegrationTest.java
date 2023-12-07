d8338c85fbcaeb41182df78545538e0b8219b247
Markushagen1 <57091267+Markushagen1@users.noreply.github.com>
2023-12-07 11:29:51 +0100

    integration service test on address and customer

--- src/test/java/com/example/exam/Address/AddressServiceIntegrationTest.java
+++ src/test/java/com/example/exam/Address/AddressServiceIntegrationTest.java
@@ -1,10 +1,19 @@
 package com.example.exam.Address;
 
+import com.example.exam.Model.Address;
+import com.example.exam.Repo.AddressRepository;
 import com.example.exam.Service.AddressService;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 
 @SpringBootTest
@@ -13,16 +22,40 @@ public class AddressServiceIntegrationTest {
     @Autowired
     private AddressService addressService;
 
+    @Autowired
+    private AddressRepository addressRepository;
+
     @Test
     @Transactional
-    void shouldFetchAddresses(){
+    void shouldGetPaginatedAddresses() {
 
-        var addresses = addressService.getAddress();
+        Address aTestAddress = new Address();
+        addressRepository.save(aTestAddress);
 
-        /*assert addresses.size() == 1;
-        assert addresses.get(0).getCustomers() */
+        Page<Address> addressesPage = addressService.getPaginatedAddresses(0, 10);
+        List<Address> addresses = addressesPage.getContent();
+
+        assertNotNull(addresses);
+        assertFalse(addresses.isEmpty());
+    }
+
+        @Test
+        @Transactional
+        void shouldFetchAddresses(){
+
+            Address aNewAddress = new Address();
+            Address savedAddress = addressService.addAddress(aNewAddress);
+
+            Address foundAddress = addressService.getAddressById(savedAddress.getAddressId());
+
+
+            assertNotNull(foundAddress);
+            assertEquals(savedAddress.getAddressId(), foundAddress.getAddressId());
+
+
+        }
 
 
     }
 
-}
+
